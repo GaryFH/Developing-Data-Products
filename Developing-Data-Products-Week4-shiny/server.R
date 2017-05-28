3b95e435daafab79cbb6cf429c8d54b1a2820981
Gary Hulbert <Gary Hulbert>
2017-05-27 17:58:16 -0700

    final commit?

--- Developing-Data-Products-Week4-shiny/server.R
+++ Developing-Data-Products-Week4-shiny/server.R
@@ -12,11 +12,7 @@ shinyServer(function(input, output){
     d1<-tbl_df(diamonds)
     d2<-select(d1,price,carat:table) ## made price first column
     diamonds<-as.data.frame(d2)
-    a1<-mtcars
-    a2<-airquality
-    a3<-as.data.frame(d2)##diamonds
-    a4<-state<-as.data.frame(state.x77)
-
+  
     BestFit<-function(x,y,z) {               
       x1<-x[,-1]
       
@@ -59,22 +55,64 @@ shinyServer(function(input, output){
       ## rownames(aa5)<-rep("",nrow(aa5))
     }
     
-    react1<- reactive({ 
+    react1<- eventReactive(input$go,{ 
       
-      if(input$MTCARS){tabl1<-BestFit(mtcars,input$sliderNumVar,input$CRITERA)
+      if(input$MTCARS){tabl1<-BestFit(mtcars,input$sliderNumVar,input$CRITERIA)
       }
-      if(input$AIRQUALITY){tabl1<-BestFit(airquality,input$sliderNumVar,input$CRITERA)
+      if(input$AIRQUALITY){tabl1<-BestFit(airquality,input$sliderNumVar,input$CRITERIA)
       }
-      if(input$DIAMONDS){tabl1<-BestFit(diamonds,input$sliderNumVar,input$CRITERA)
+      if(input$DIAMONDS){tabl1<-BestFit(diamonds,input$sliderNumVar,input$CRITERIA)
       }
-      if(input$STATE.X77){tabl1<-BestFit(state<-as.data.frame(state.x77),input$sliderNumVar,input$CRITERA)
+      if(input$STATE.X77){tabl1<-BestFit(state<-as.data.frame(state.x77),input$sliderNumVar,input$CRITERIA)
       }
       tabl1
-    })          
+    }) 
     
-    output$tabl<-renderDataTable({react1()})
-
-}) 
+    react2<- reactive({ 
+            
+            if(input$MTCARS){text1<-"mtcars database"
+            }
+            if(input$AIRQUALITY){text1<-"airquality database"
+            }
+            if(input$DIAMONDS){text1<-"diamonds database"
+            }
+            if(input$STATE.X77){text1<-"state.x77 database"
+            }
+            text1
+   })   
+    
+    react3<- reactive({ 
+            
+            ifelse(input$CRITERIA==1,text3<-"criteria is Adjusted R Squared",text3<-"Criteria is P Value")
+            
+            text3
+    }) 
+    react4<- reactive({ 
+            
+            if(input$MTCARS){text4<-"Conditional Variable is Miles per gallon"
+            }
+            if(input$AIRQUALITY){text4<-"Conditional Variable is Ozone level"
+            }
+            if(input$DIAMONDS){text4<-"Conditional Variable is Price"
+            }
+            if(input$STATE.X77){text4<-"Conditional Variable is Population"
+            }
+            text4
+    }) 
+    
+    
+        output$tabl<-renderDataTable({react1()})
+       
+        output$text1 <- renderText({react2()
+   })
+        output$text2 <- renderText({paste("You have selected the", react2())
+   })
+        output$text3 <- renderText({paste("The method", react3())
+   })
+        output$text4 <- renderText({react4()
+   })
+   
+   }) 
                 
                 
       
--- Developing-Data-Products-Week4-shiny/ui.R
+++ Developing-Data-Products-Week4-shiny/ui.R
@@ -4,19 +4,39 @@
 library(shiny)
 
 shinyUI(fluidPage(
-  titlePanel("Find Best Predictor Variables"),
+        titlePanel("Find Best Predictor Variables"),
+        h4("Introduction"),
+        h5("This application uses four different 
+           databases available from R programming 
+           language and uses two different statistical 
+           methods to predict the conditional variable"),
+        h4("Methods"),
+        h5("I use a linear model to the best regressors 
+           for predicting the conditional varible.  The 
+           table contains the information about the best 
+           regressors and the conditional variable is 
+           also displayed."),
   sidebarLayout(
     sidebarPanel(
-      sliderInput("sliderNumVar", "How many predictors do you want", 1, 4, value = 2, step = 1),
+      sliderInput("sliderNumVar", "How many predictors do you want", 1, 10, value = 4, step = 1),
       checkboxInput("MTCARS", "mtcars data base", value = T),
       checkboxInput("AIRQUALITY", "airquality data base", value = F),
       checkboxInput("DIAMONDS", "diamonds data base", value = F),
       checkboxInput("STATE.X77", "state.x77 data base", value = F),
-      selectInput("CRITERA","Criteria",c("PVALUE" = 2,"AdjustedR" = 1))
+      selectInput("CRITERIA","Criteria",c("PVALUE" = 2,"AdjustedR" = 1)),
+      actionButton("go", "Press to SUBMIT your choices")
+      
     ),
     mainPanel(
-      h3("hello"),
-      dataTableOutput("tabl")
+      h2("Output From BestFit function "),
+      h4("You can choose from four different datasets and from two different statistical criteria in order to determine which variables (regressors) best predicts the conditional variable"),
+      h4("The table below displays the variables that best predict the conditional variable and indicates both the Pvalue and Adjusted Squared R value"),
+      h4("After selecting your options you must press the SUBMIT button to proceed"),
+      dataTableOutput("tabl"),
+      h4(textOutput("text2")),
+      h4(textOutput("text3")),
+      h4(textOutput("text4"))
+      
     )
   )
 ))
